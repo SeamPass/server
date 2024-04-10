e993b3262d7abf12f008d74ea79afa9851be8e03
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2024-04-10 22:41:56 +0100

    reversing changes

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 require("dotenv").config();
@@ -11,34 +13,49 @@ const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const error_1 = require("./src/middleware/error");
 const user_route_1 = __importDefault(require("./src/routes/user.route"));
-const password_route_1 = __importDefault(require("./src/routes/password.route"));
-const encryptionKey_route_1 = __importDefault(require("./src/routes/encryptionKey.route"));
+const password_route_1 = __importDefault(
+  require("./src/routes/password.route")
+);
+const encryptionKey_route_1 = __importDefault(
+  require("./src/routes/encryptionKey.route")
+);
 const secret_route_1 = __importDefault(require("./src/routes/secret.route"));
 const wifi_route_1 = __importDefault(require("./src/routes/wifi.route"));
 //body parser
-exports.app.use(express_1.default.json({
+exports.app.use(
+  express_1.default.json({
     limit: "50mb",
-}));
+  })
+);
 //cookie parser
 exports.app.use((0, cookie_parser_1.default)());
 //cors => cross origin resource sharing
-exports.app.use((0, cors_1.default)({
+exports.app.use(
+  (0, cors_1.default)({
     origin: ["http://localhost:5173", "http://localhost:3000"],
     credentials: true,
-}));
+  })
+);
 //routes
-exports.app.use("/api/v1", user_route_1.default, password_route_1.default, secret_route_1.default, wifi_route_1.default, encryptionKey_route_1.default);
+exports.app.use(
+  "/api/v1",
+  user_route_1.default,
+  password_route_1.default,
+  secret_route_1.default,
+  wifi_route_1.default,
+  encryptionKey_route_1.default
+);
 //testing API
 exports.app.get("/test", (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: "Api is working perfectly",
-    });
+  res.status(200).json({
+    success: true,
+    message: "Api is working perfectly",
+  });
 });
 //unknown route incase of error
 exports.app.all("*", (req, res, next) => {
-    const err = new Error(`Route ${req.originalUrl} not found`);
-    err.statusCode = 400;
-    next(err);
+  const err = new Error(`Route ${req.originalUrl} not found`);
+  err.statusCode = 400;
+  next(err);
 });
 exports.app.use(error_1.ErrorMiddleware);
--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const db_1 = __importDefault(require("./src/utils/db"));
@@ -9,6 +11,6 @@ require("dotenv").config();
 //create server
 const port = process.env.PORT;
 app_1.app.listen(port, () => {
-    console.log(`Server is connected with port ${port}`);
-    (0, db_1.default)();
+  console.log(`Server is connected with port ${port}`);
+  (0, db_1.default)();
 });
