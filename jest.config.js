313a4b10f8b7f590d16a0c5b1b5ad2df7e6ea16e
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2024-04-11 00:46:06 +0100

    Adjust TypeScript configuration for build

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
-  transform: {
-    "^.+\\.(t|j)sx?$": "ts-jest",
-  },
-  globals: {
-    "ts-jest": {
-      tsconfig: "./tsconfig.test.json",
-    },
-  },
+  testEnvironment: "node",
 };
--- src/__test__/password.test.ts
+++ src/__test__/password.test.ts
@@ -1,128 +1,128 @@
-// jest.mock("../models/password.model", () => {
-//   const mockModel = {
-//     findOne: jest.fn(),
-//     create: jest.fn(),
-//     // Other methods as needed
-//   };
-//   return {
-//     __esModule: true,
-//     default: mockModel,
-//     // Export the mocked model directly
-//     PasswordModel: mockModel,
-//   };
-// });
+jest.mock("../models/password.model", () => {
+  const mockModel = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    // Other methods as needed
+  };
+  return {
+    __esModule: true,
+    default: mockModel,
+    // Export the mocked model directly
+    PasswordModel: mockModel,
+  };
+});
 
-// import mongoose from "mongoose";
-// import { Request, Response, NextFunction } from "express";
-// import {
-//   addPassword,
-//   getSinglePassword,
-// } from "../controllers/password.controller";
-// import PasswordModel from "../models/password.model";
+import mongoose from "mongoose";
+import { Request, Response, NextFunction } from "express";
+import {
+  addPassword,
+  getSinglePassword,
+} from "../controllers/password.controller";
+import PasswordModel from "../models/password.model";
 
-// type MockRequest = Partial<Request> & {
-//   params: { id: string };
-//   user: { _id: string };
-//   body: any;
-// };
+type MockRequest = Partial<Request> & {
+  params: { id: string };
+  user: { _id: string };
+  body: any;
+};
 
-// interface MockResponse extends Partial<Response> {
-//   json: jest.Mock;
-//   status: jest.Mock;
-// }
+interface MockResponse extends Partial<Response> {
+  json: jest.Mock;
+  status: jest.Mock;
+}
 
-// interface MockNextFunction extends NextFunction {}
+interface MockNextFunction extends NextFunction {}
 
-// describe("PasswordController", () => {
-//   let mockReq: MockRequest;
-//   let mockRes: MockResponse;
-//   let mockNext: MockNextFunction;
+describe("PasswordController", () => {
+  let mockReq: MockRequest;
+  let mockRes: MockResponse;
+  let mockNext: MockNextFunction;
 
-//   beforeEach(() => {
-//     mockReq = {
-//       params: { id: "password_id" },
-//       user: { _id: "312y31sh373" },
-//       body: {},
-//     } as any;
+  beforeEach(() => {
+    mockReq = {
+      params: { id: "password_id" },
+      user: { _id: "312y31sh373" },
+      body: {},
+    } as any;
 
-//     mockRes = {
-//       json: jest.fn(() => mockRes),
-//       status: jest.fn(() => mockRes),
-//     } as MockResponse;
+    mockRes = {
+      json: jest.fn(() => mockRes),
+      status: jest.fn(() => mockRes),
+    } as MockResponse;
 
-//     mockNext = jest.fn() as MockNextFunction;
+    mockNext = jest.fn() as MockNextFunction;
 
-//     jest.clearAllMocks();
-//   });
+    jest.clearAllMocks();
+  });
 
-//   describe("getSinglePassword", () => {
-//     it("should get a password by id", async () => {
-//       const passwordData = {
-//         _id: new mongoose.Types.ObjectId(),
-//         user: mockReq.user?._id,
-//         websiteName: "Test Website",
-//         url: "http://test.com",
-//         username: "testUser",
-//         password: "testPass",
-//         passwordStrength: "Strong",
-//         compromised: false,
-//       };
+  describe("getSinglePassword", () => {
+    it("should get a password by id", async () => {
+      const passwordData = {
+        _id: new mongoose.Types.ObjectId(),
+        user: mockReq.user?._id,
+        websiteName: "Test Website",
+        url: "http://test.com",
+        username: "testUser",
+        password: "testPass",
+        passwordStrength: "Strong",
+        compromised: false,
+      };
 
-//       (PasswordModel.findOne as jest.Mock).mockResolvedValue(passwordData);
+      (PasswordModel.findOne as jest.Mock).mockResolvedValue(passwordData);
 
-//       await getSinglePassword(
-//         mockReq as unknown as Request,
-//         mockRes as unknown as Response,
-//         mockNext as NextFunction
-//       );
+      await getSinglePassword(
+        mockReq as unknown as Request,
+        mockRes as unknown as Response,
+        mockNext as NextFunction
+      );
 
-//       expect(PasswordModel.findOne).toHaveBeenCalled();
-//       expect(mockRes.json).toHaveBeenCalledWith(expect.anything());
-//     });
-//   });
+      expect(PasswordModel.findOne).toHaveBeenCalled();
+      expect(mockRes.json).toHaveBeenCalledWith(expect.anything());
+    });
+  });
 
-//   describe("addPassword", () => {
-//     it("should add a new password if not already existing", async () => {
-//       const passwordDetails = {
-//         websiteName: "New Website",
-//         url: "https://new-website.com",
-//         username: { encUsername: "username", iv: "usernameIv" },
-//         password: { encPassword: "password", iv: "passwordIv" },
-//       };
+  describe("addPassword", () => {
+    it("should add a new password if not already existing", async () => {
+      const passwordDetails = {
+        websiteName: "New Website",
+        url: "https://new-website.com",
+        username: { encUsername: "username", iv: "usernameIv" },
+        password: { encPassword: "password", iv: "passwordIv" },
+      };
 
-//       mockReq.body = { ...passwordDetails, websiteUrl: passwordDetails.url };
+      mockReq.body = { ...passwordDetails, websiteUrl: passwordDetails.url }; // Add websiteUrl property for consistency with the controller function
 
-//       // Mock implementation of findOne to simulate no existing password
-//       (PasswordModel.findOne as jest.Mock).mockResolvedValue(null);
+      // Mock implementation of findOne to simulate no existing password
+      (PasswordModel.findOne as jest.Mock).mockResolvedValue(null);
 
-//       // Mock implementation of create
-//       (PasswordModel.create as jest.Mock).mockResolvedValue({
-//         user: mockReq.user?._id,
-//         ...passwordDetails,
-//         passwordStrength: "Normal",
-//         compromised: false,
-//       });
+      // Mock implementation of create
+      (PasswordModel.create as jest.Mock).mockResolvedValue({
+        user: mockReq.user?._id,
+        ...passwordDetails,
+        passwordStrength: "Normal",
+        compromised: false,
+      });
 
-//       // Act
-//       await addPassword(
-//         mockReq as unknown as Request,
-//         mockRes as unknown as Response,
-//         mockNext as NextFunction
-//       );
+      // Act
+      await addPassword(
+        mockReq as unknown as Request,
+        mockRes as unknown as Response,
+        mockNext as NextFunction
+      );
 
-//       // Assert
-//       expect(PasswordModel.findOne).toHaveBeenCalledWith({
-//         user: mockReq.user?._id,
-//         websiteName: passwordDetails.websiteName,
-//         url: passwordDetails.url,
-//       });
+      // Assert
+      expect(PasswordModel.findOne).toHaveBeenCalledWith({
+        user: mockReq.user?._id,
+        websiteName: passwordDetails.websiteName,
+        url: passwordDetails.url,
+      });
 
-//       expect(PasswordModel.create).toHaveBeenCalledWith(expect.anything());
-//       // expect(mockRes.status).toHaveBeenCalledWith(201);
-//       // expect(mockRes.json).toHaveBeenCalledWith({
-//       //   success: true,
-//       //   message: "Password information added successfully.",
-//       // });
-//     });
-//   });
-// });
+      expect(PasswordModel.create).toHaveBeenCalledWith(expect.anything());
+      // expect(mockRes.status).toHaveBeenCalledWith(201);
+      // expect(mockRes.json).toHaveBeenCalledWith({
+      //   success: true,
+      //   message: "Password information added successfully.",
+      // });
+    });
+  });
+});
