d229d8c78989de0ee4da48ae182a89fffb1908c6
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2024-04-11 01:15:37 +0100

    Adjust TypeScript configuration for build

--- dist/src/utils/minutesToFutureTimestamp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.minutesToFutureTimestamp = void 0;
-function minutesToFutureTimestamp(milliseconds) {
-    const currentTime = new Date().getTime();
-    const futureTime = currentTime + milliseconds;
-    return Math.round(futureTime / 1000);
-}
-exports.minutesToFutureTimestamp = minutesToFutureTimestamp;
